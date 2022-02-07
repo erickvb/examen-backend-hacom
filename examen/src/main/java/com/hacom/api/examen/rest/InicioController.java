58fd9a4d7b49f2b12dfdb1ef5db33e5ea8720288
erickvb <anveb_7@hotmail.com>
2022-02-06 21:30:24 -0500

    Update InicioController.java

--- examen/src/main/java/com/hacom/api/examen/rest/InicioController.java
+++ examen/src/main/java/com/hacom/api/examen/rest/InicioController.java
@@ -3,23 +3,21 @@ package com.hacom.api.examen.rest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hacom.api.examen.converter.TraceMsgConverter;
+
 import com.hacom.api.examen.dto.TraceMsgDto;
 import com.hacom.api.examen.dto.TraceMsgRequest;
-import com.hacom.api.examen.entity.TraceMsg;
 import com.hacom.api.examen.input.DateRangeRequest;
 import com.hacom.api.examen.output.WrapperResponse;
-import com.hacom.api.examen.output.WrapperResponseFlux;
 import com.hacom.api.examen.service.TraceMsgService;
 import com.hacom.api.examen.util.UtilEnum.STATUS_OPERATION;
+import com.hacom.api.examen.config.MetricsPrometheus;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -36,10 +34,14 @@ public class InicioController {
 	@Autowired
 	private TraceMsgService traceMsgService;
 	
+	@Autowired
+	private  MetricsPrometheus metricsPrometheus;
+	
 	
 	@PostMapping()
 	public  Mono<WrapperResponse<TraceMsgDto>>  save(@RequestBody TraceMsgRequest traceMsg) {
 		logger.info("POST REQUEST {}",traceMsg);
+		metricsPrometheus.addCountRequest();
 		return traceMsgService.saveTrace(traceMsg).map(g ->{ 
 			
 			return new WrapperResponse<TraceMsgDto>(STATUS_OPERATION.SUCCESS.getStatus(),STATUS_OPERATION.SUCCESS.getMessage(), g);
